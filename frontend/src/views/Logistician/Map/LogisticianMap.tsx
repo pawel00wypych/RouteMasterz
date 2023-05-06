0d6dc3b443f42228fbf6012ae5872cf99e76aa77
pawel_wypych <pawelwypych4@gmail.com>
2023-05-06 11:34:34 +0200

    address searching, v.0

--- frontend/src/views/Logistician/Map/LogisticianMap.tsx
+++ frontend/src/views/Logistician/Map/LogisticianMap.tsx
@@ -21,19 +21,139 @@ import MainContainer from "../../../components/Containers/MainContainer/MainCont
 import TopBar from "../../../components/TopBar/TopBar";
 import MidContainer from "../../../components/Containers/MidContainer/MidContainer";
 import Page from "../../../components/Page/Page";
+// @ts-ignore
+import { geosearch } from 'leaflet-geosearch';
+import { useEffect, useState } from "react";
 
 function LogisticianMap() {
+    const [coords, setCorrds] = useState({
+        latitude: 39.7837304,
+        longitude: -100.4458825
+    });
+
+    const [display_name, setName] = useState("");
+
+    const [address, setAddress] = useState<{ [key: string]: string }>({
+        country: '',
+        street: '',
+        city: '',
+        state: '',
+        postalcode: '',
+    });
+
+    function error() {
+        alert('Sorry, no position available.');
+    }
+    const options = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    };
+
+    function getCurrentCityName( position: geosearch.Position){
+        setCorrds({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+
+
+        let url="https://nominatim.openstreetmap.org/reverse?format=jsonv2"+
+            "&lat="+coords.latitude+"&lon="+coords.longitude;
+
+        fetch(url, {
+            method: "GET",
+            mode: 'cors',
+            headers: {
+                "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => setName( data.display_name));
+    }
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+
+            getCurrentCityName,
+            error,
+            options
+
+        );
+    }, []);
+
+
+    //separete the entred string
+    function update(field: string) {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            const value = e.currentTarget.value;
+            setAddress((address) => ({ ...address, [field]: value }));
+        };
+    }
+
+    function submitHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+
+        e.preventDefault();
+        console.log(address);
+
+        let url = `https://nominatim.openstreetmap.org/search?
+    street=${address.street}
+    &city=${address.city}
+    &state=${address.state}
+    &country=${address.country}
+    &postalcode=${address.postalcode}&format=json`;
+
+        fetch(url, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
+            }
+        })
+            .then((response ) => {
+                if( response.ok){
+                    return response.json();
+                }
+                throw new Error('Something went wrong');
+            })
+            .then(
+                (data) => {
+                    setName(data[0].display_name);
+                    setCorrds({
+                        latitude: data[0].lat,
+                        longitude: data[0].lon
+                    });
+                }
+
+            ).catch((error) => {
+            alert("Error in your input; unable to find the position");
+        });
+    }
+
 
     return (
         <Page>
             <MainContainer>
                 <TopBar/>
                 <MidContainer>
-                    <CustomContainer className={style.mapBox}>
-                        <img className={style.mapSrc} src={cityMap} alt={""}/>
-                    </CustomContainer>
+                    <Map coords={coords} display_name={display_name}/>
                     <CustomContainer className={style.mapRightPulpit}>
-                        <FormInput className={inputStyle.mapSearch} text={"plac Defilad 1, 00-901 Warszawa"}></FormInput>
+                        <FormInput className={inputStyle.mapSearch}
+                                   text={"Country"}
+                                   value={address.country}
+                                   onChange={update("country")}></FormInput>
+                        <FormInput className={inputStyle.mapSearch}
+                                   text={"Street"}
+                                   value={address.street}
+                                   onChange={update("street")}></FormInput>
+                        <FormInput className={inputStyle.mapSearch}
+                                       text={"City"}
+                                       value={address.city}
+                                       onChange={update("city")}></FormInput>
+                        <FormInput className={inputStyle.mapSearch}
+                                       text={"Zip Code"}
+                                       value={address.postalcode}
+                                       onChange={update("postalcode")}></FormInput>
+
                         <CustomContainer className={style.addedPoints}>
                             <CustomContainer className={style.addPoint}>
                                 <FormInput className={inputStyle.chooseHour} text={"14:30"}></FormInput>
@@ -47,7 +167,8 @@ function LogisticianMap() {
                         <Button className={buttonStyle.saveRouteButton}
                                 src={save}
                                 iconHeight={"60px"}
-                                iconWidth={"60px"}/>
+                                iconWidth={"60px"}
+                                onClick={(e) => submitHandler(e)}/>
                     </CustomContainer>
                 </MidContainer>
             </MainContainer>
--- frontend/src/views/Map/Map.tsx
+++ frontend/src/views/Map/Map.tsx
@@ -3,6 +3,8 @@ import TopBar from "../../components/TopBar/TopBar";
 import MainContainer from "../../components/Containers/MainContainer/MainContainer";
 import MidContainer from "../../components/Containers/MidContainer/MidContainer";
 import CustomContainer from "../../components/Containers/CustomContainer/CustomContainer";
+// @ts-ignore
+import icon from "../../resources/img/pin.svg";
 
 // @ts-ignore
 import cityMap from "../../resources/img/cityMap.svg";
@@ -10,34 +12,57 @@ import cityMap from "../../resources/img/cityMap.svg";
 import style from "./Map.module.css";
 
 import {Component, HTMLAttributes} from "react";
+// @ts-ignore
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import React from "react";
+import "leaflet/dist/leaflet.css";
+// @ts-ignore
+import L from "leaflet";
 
 interface MapProps {
-    title?: string;
-    background?: boolean;
-    contentClassName?: string;
+    coords?: {};
+    display_name?: string;
     children?: HTMLAttributes<HTMLDivElement>['children'];
 }
-export default class Map extends Component<MapProps> {
-    private static defaultProps: MapProps = {
-        title: '',
-        background: false,
-        contentClassName: '',
-    };
-    render()
-    {
-        return (
-            <Page>
-                <MainContainer>
-                    <TopBar/>
-                    <MidContainer>
-                        <CustomContainer className={style.mapBox}>
-                            <img className={style.mapSrc} src={cityMap} alt={""}/>
-                        </CustomContainer>
-                        <CustomContainer className={style.mapRightPulpit}>
-                        </CustomContainer>
-                    </MidContainer>
-                </MainContainer>
-            </Page>
-        );
+
+export default function Map({coords, display_name }: MapProps) {
+
+    // @ts-ignore
+    let longitude = coords.longitude;
+    // @ts-ignore
+    let latitude = coords.latitude;
+
+    console.log(latitude);
+    console.log(longitude);
+
+    const customIcon = new L.Icon({
+        iconUrl: icon,
+        iconSize: [25, 35],
+        iconAnchor: [5, 30]
+    });
+
+    function MapView() {
+        let map = useMap();
+        map.setView([latitude, longitude], map.getZoom());
+
+        return null;
     }
+
+        return (
+                        <MapContainer
+                            className={style.mapBox}
+                            center={[latitude, longitude]}
+                            zoom={5}
+                            scrollWheelZoom={true}>
+                            <TileLayer
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>
+                                contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[latitude, longitude]}>
+                                <Popup>{display_name}</Popup>
+                            </Marker>
+                            <MapView />
+                        </MapContainer>
+        );
 }
